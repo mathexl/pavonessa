f77a44d121951b01b7fd5cba3079b86602c5639b
Mathew <mdp2163@columbia.edu>
2015-11-04 11:59:21 -0500

    adding mysql connection

--- core/setup/index.php
+++ core/setup/index.php
@@ -8,23 +8,41 @@ TO RESET CMS.  UNCOMMENT THE REST OF THE FILE BELOW BEFORE YOU RUN IT.
 v0.0.1, November 4th, 2015
 *****************************************************************************/
 
-
-
 if($_POST){
+
   if($_POST['host'] && $_POST['username'] && $_POST['password'] && $_POST['db_name']){
 
-
     try {
-        return new PDO('mysql:host=' . $_POST['host'] . ';dbname=' . $_POST['db_name'],
+        $db = new PDO('mysql:host=' . $_POST['host'] . ';dbname=' . $_POST['db_name'],
         $_POST['username'], $_POST['password']);
+
     } catch (PDOException $e){
-         echo 'Connection failed: ' . $e->getMessage();
+        include_once("config.php?failed=true"); //if db vars not there.
     }
 
+    /* This assumes the connection worked */
+    /*
+    $stmnt = $db->prepare("
+
+        CREATE TABLE users (
+          `_id` VARCHAR(60),
+          `fname` VARCHAR(100),
+          `lname` VARCHAR(100),
+          `type` VARCHAR(100),
+          `sessionid1` VARCHAR(100),
+          `sessionid2` VARCHAR(100),
+          `lastloggedin` VARCHAR(100)
+        );
+
+    ");
+
+    $stmnt->execute();
+    */
 
   } else {
   include_once("config.php?error=true"); //if db vars not there.
-   }
+  }
+
 } else {
   include_once("config.php"); // otherwise.
 }
